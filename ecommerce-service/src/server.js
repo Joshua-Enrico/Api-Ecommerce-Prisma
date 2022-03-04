13dacdc6bdebea926ed849c055a520956511463a
Joshua-Enrico <joshuaenrico123@gmail.com>
2022-03-03 22:54:54 -0500

    new endpoints

--- ecommerce-service/src/server.js
+++ ecommerce-service/src/server.js
@@ -2,6 +2,10 @@ const express = require('express');//import express
 const cors = require('cors');// import cors
 require('dotenv').config();// Set up dotenv
 
+// Importamos los endpoints
+const adminRoutes = require('./routes/routesAdmin/admin');
+const userSellerRoutes = require('./routes/routesUserSeller/userSeller');
+const sellerRoutes = require('./routes/routesSeller/seller');
 // Create a new express application instance
 const api = express();
 
@@ -9,9 +13,16 @@ const api = express();
 api.use(express.json());
 api.use(cors());
 
+// Test api
+api.get('/', (req, res) => {
+    res.send('API is running');
+})
+
 
 // Configure routes
-
+api.use('/ecommerce/admin', adminRoutes);
+api.use('/ecommerce/userSeller', userSellerRoutes);
+api.use('/ecommerce/seller', sellerRoutes);
 
 
 // Start the server
