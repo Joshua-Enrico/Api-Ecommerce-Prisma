0f2a11486a43b301bca51ac09afa45aef2950d24
Joshua-Enrico <joshuaenrico123@gmail.com>
2022-03-03 23:00:21 -0500

    Auth Endpoints

---
+++ ecommerce-service/src/routes/routesAuth/auth.js
@@ -0,0 +1,11 @@
+// Este modulo es el encargado de manejar las rutas de autenticacion
+// Registar usuario y login a su vez de proporcionar un token de autenticacion
+
+const router = require('express').Router();
+
+
+
+/*  Login de usuario , al manejar tablas
+    separadas para los tipos de usuarios 
+     */
+router.post('/login/', (req, res) => {})
