74959301f37161107d697586f19ab731cfdd243f
Joshua-Enrico <joshuaenrico123@gmail.com>
2022-03-05 12:12:32 -0500

    userSeller endpoints

--- ecommerce-service/src/routes/routesUserSeller/userSeller.js
+++ ecommerce-service/src/routes/routesUserSeller/userSeller.js
@@ -2,6 +2,10 @@ const { checkUserExistance } = require('../globalQueries/checkUserExistance');
 const { dinamycVal } = require('../globalValidations/dinamycArgsValidation');
 const { errUsrSellerMsg } = require('../utils/messages');
 const { createSellerUser } = require('./queries/createSellerUser');
+const { getAllSellersUsr } = require('./queries/getAllSellerUsers');
+const { getSellerUserById } = require('./queries/getSellerUsrById');
+const { searchUserSellers } = require('./queries/searchSellerUser');
+const { updateSellerUsr } = require('./queries/updateSellerUsr');
 
 const router = require('express').Router();
 
@@ -19,16 +23,41 @@ router.post('/', async (req, res) => {
 
 
 // Listar Usuarios
-router.get('/all', (req, res) => {})
+router.get('/all', (req, res) => {
 
-// Eliminar Usuario
-router.delete('/:id', (req, res) => {})
+    getAllSellersUsr(req, res);
+
+})
+
+// Buscar Usuario por id
+router.get('/:id', (req, res) => {
+
+    getSellerUserById(req, res);
+
+})
+
+// Eliminar Usuario, no es buena practica eliminar un usuario, se debe desactivar
+router.delete('/:id', (req, res) => {
+
+})
 
 // Actualizar Usuario
-router.put('/:id', (req, res) => {})
+router.put('/:id', (req, res) => {
+
+    const validArgs = ["sellerId", "name", "password", "email"]
+    const err = dinamycVal(req.body, res, validArgs, 4, errUsrSellerMsg, true);// Valida los argumentos
+    if (!err) {
+        updateSellerUsr(req, res);
+    }
+
+})
 
 // Buscar Usuario
-router.get('/search/:search', (req, res) => {})
+router.get('/search/:search', (req, res) => {
+
+    searchUserSellers(req, res);
+
+})
 
 
 module.exports = router;
